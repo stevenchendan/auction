c2d30f7e52f0c06b98c072254aac2b9e6e6aca94
stevenchen <qwe16165850@gmail.com>
2018-04-10 11:01:56 +1000

    fix the rating function

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,18 +1,24 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
   @Input() rating = 0;
   stars: boolean[];
 
   @Input() readonly = true;
   constructor() { }
 
+  @Output() ratingChange: EventEmitter<number> = new EventEmitter();
+
   ngOnInit() {
+
+  }
+
+  ngOnChanges() {
     this.stars = [];
     for (let i = 1; i <= 5; i++) {
       this.stars.push(i > this.rating);
@@ -21,9 +27,10 @@ export class StarsComponent implements OnInit {
 
   clickStar(index) {
     if (!this.readonly) {
-      console.log('index', index);
       this.rating = index + 1;
     }
+
+    this.ratingChange.emit(this.rating);
   }
 
 }
